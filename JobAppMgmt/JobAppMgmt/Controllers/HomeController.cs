601e24e1ca8ea25eeecfcbbb784fb6fa01ab1af8
Neha <28neha@gmail.com>
2016-07-18 22:22:24 -0500

    layout

---
+++ JobAppMgmt/JobAppMgmt/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobAppMgmt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Status()
+        {
+            ViewBag.Message = "The companies you have applied so far are listed below: ";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Feel free to reach out to us!";
+
+            return View();
+        }
+    }
+}
